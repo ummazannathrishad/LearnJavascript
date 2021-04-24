0282b3a268931c5f887f3658230cff793704087b
ummazannathrishad <ummazannathrishad755@gmail.com>
2021-04-24 14:28:53 +0600

    updated

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//document.getElementById("root").innerHTML="i love javascript!";
+    /*document.getElementById("idH1").innerHTML="I am a good girl";
+    document.getElementById("idH2").innerHTML="I am learning Javascript";*/
+    //console.log("i am a student!");
+    var x;
+    x=prompt("Enter you name:");
+    console.log(x);
+    document.write(x);
